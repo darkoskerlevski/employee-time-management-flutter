846eb93aff3ebaa7ee0a71e6df897c37805fd743
Darko Skerlevski <darkoskerlevski@gmail.com>
2022-03-31 21:46:02 +0200

    fix merge conflict

--- lib/screens/my_tasks_screen.dart
+++ lib/screens/my_tasks_screen.dart
@@ -60,12 +60,7 @@ class _MyTasksScreenState extends State<MyTasksScreen> {
         });
       });
   }
-
-  @override
-  void initState() {
-    widget.myTasks.add(new Task(id: "darkoID", allocatedTo: "e6ielq8FESgiFet2uxD1YXJHvYQ2", companyId: "a74647c0-b061-11ec-b2a3-bdfc99ff3e74", title: "Task Title", description: "Task Description", by: DateTime.now()));
-    super.initState();
-  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
