9f084d01bfa8bb14c31f50038614a6a499a1a16b
Jungelr <viktor.panovski@yahoo.com>
2022-04-01 23:13:52 +0200

    Level sensor data

--- lib/model/Task.dart
+++ lib/model/Task.dart
@@ -8,7 +8,9 @@ class Task{
   int timeSpent;
   bool stopwatchPressed;
   DateTime stopwatchLastPress;
+  double sumRoll;
+  double sumPitch;
 
-  Task({required this.id,required this.allocatedTo,required this.companyId,required this.title,required this.description,required this.by,required this.timeSpent,required this.stopwatchPressed,required this.stopwatchLastPress});
+  Task({required this.sumPitch,required this.sumRoll,required this.id,required this.allocatedTo,required this.companyId,required this.title,required this.description,required this.by,required this.timeSpent,required this.stopwatchPressed,required this.stopwatchLastPress});
 
 }
